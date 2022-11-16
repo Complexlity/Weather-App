7efa067666ad05d754d33e47037dbdcbe4d1f4d7
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-16 20:58:55 +0100

    Add background and gif to content

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ let dq = document.querySelector.bind(document)
 let dqa = document.querySelectorAll.bind(document)
 let c = console.log.bind(document)
 
+let root = dq(':root')
+
 let search = dqa('[data-search]')
 let cityName = dq('[data-cityName]')
 let time = dq('[data-time]')
@@ -16,8 +18,7 @@ let weatherIcon = dq('[data-weatherIcon]')
 let gif = dq('[data-gif]')
 
 
-c(weatherIcon)
-  loadSite('Lagos')
+  loadSite('somerset')
 
 
 async function loadSite(cityName){
@@ -34,14 +35,24 @@ function updatePage(data){
     data = weatherData[0]
     let iconCode = data.weather[0].icon
     let today = new Date()
+    let backgroundImage;
+    let textColor;
+    let weatherArray = ['rain', 'clouds', 'clear', 'mist']
+    let weatherName = weatherData[1].toLowerCase()
+    backgroundImage = `url('../assets/${weatherName}.jpg')`
+    if(!(weatherArray.includes(weatherName))) backgroundImage = '#111827'
+    root.style.setProperty('--bg-image', backgroundImage)
+   
     cityName.innerHTML = data.name;
     time.innerHTML = `Today ${today.getHours()}:${today.getMinutes()}`
     temperature.innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°";
     weatherIcon.src = "http://openweathermap.org/img/w/" + iconCode + ".png"
+    weatherIcon.style.display = 'inline-block'
     weather.innerHTML = weatherData[1]
     windPressure.innerHTML = data.main.pressure + 'hpa'
     humidity.innerHTML = data.main.humidity + '%'
     windSpeed.innerHTML = data.wind.speed + 'm/s'
+    gif.src = backgroundGif
     //   document.getElementById("location").innerHTML = data.name;
     //   document.getElementById("description").innerHTML = data.weather[0].description;
     //   document.getElementById("data_temperature").innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°C";
@@ -51,11 +62,6 @@ function updatePage(data){
     //   document.getElementById("data_pressure").innerHTML = data.main.pressure + "hPa";
     //   document.getElementById("data_sunset").innerHTML = new Date(data.sys.sunset*1000).toLocaleTimeString();
     // ;
-    let backgroundImage;
-    let textColor;
-    let weatherArray = ['rain', 'clouds', 'clear', 'mist']
-    let weatherName = weatherData[1].toLowerCase()
-    backgroundImage = `url('../assets/${weatherName}.jpg')`
-    if(!(weatherArray.includes(weatherName))) backgroundImage = '#111827'
+
 
   }
