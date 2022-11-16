8912949e0a26c49ff218ea96accfdf435508e369
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-16 20:58:40 +0100

    Add weather data to content

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,6 @@ async function getRequest(cityName){
   let request = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=20f7632ffc2c022654e4093c6947b4f4` 
   let response = await fetch(request)
   let data = await response.json()
-  console.log(data)
 let currentWeather = data.weather[0].main
 return [data, currentWeather]
 }
--- js/main.js
+++ js/main.js
@@ -4,31 +4,58 @@ let dq = document.querySelector.bind(document)
 let dqa = document.querySelectorAll.bind(document)
 let c = console.log.bind(document)
 
-loadSite()
+let search = dqa('[data-search]')
+let cityName = dq('[data-cityName]')
+let time = dq('[data-time]')
+let temperature = dq('[data-temperature]')
+let weather = dq('[data-weather]')
+let windPressure = dq('[data-windPressure]')
+let windSpeed = dq('[data-windSpeed]')
+let humidity = dq('[data-humidity]')
+let weatherIcon = dq('[data-weatherIcon]')
+let gif = dq('[data-gif]')
 
-async function loadSite(){
-    let cityName = prompt('Enter a city name')
+
+c(weatherIcon)
+  loadSite('Lagos')
+
+
+async function loadSite(cityName){
     let weatherData = await getRequest(cityName)
     let backgroundGif = await getGif(weatherData[1])
-    updatePage([weatherData[0], backgroundGif])        
+    let data = [weatherData, backgroundGif]
+    updatePage(data)
 }
 
 
-async function updatePage(dataAndImage){
-    let data = dataAndImage[0]
-    let bgUrl = dataAndImage[1]
-    document.body.style.backgroundImage = `url('${bgUrl}')`
+function updatePage(data){
+    let weatherData = data[0]
+    let backgroundGif = data[1]
+    data = weatherData[0]
+    let iconCode = data.weather[0].icon
+    let today = new Date()
+    cityName.innerHTML = data.name;
+    time.innerHTML = `Today ${today.getHours()}:${today.getMinutes()}`
+    temperature.innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°";
+    weatherIcon.src = "http://openweathermap.org/img/w/" + iconCode + ".png"
+    weather.innerHTML = weatherData[1]
+    windPressure.innerHTML = data.main.pressure + 'hpa'
+    humidity.innerHTML = data.main.humidity + '%'
+    windSpeed.innerHTML = data.wind.speed + 'm/s'
+    //   document.getElementById("location").innerHTML = data.name;
+    //   document.getElementById("description").innerHTML = data.weather[0].description;
+    //   document.getElementById("data_temperature").innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°C";
+    //   document.getElementById("data_humidity").innerHTML = data.main.humidity + "%";
+    //   document.getElementById("data_wind_speed").innerHTML = data.wind.speed + "m/s";
+    //   document.getElementById("data_wind_direction").innerHTML = data.wind.deg + "º";
+    //   document.getElementById("data_pressure").innerHTML = data.main.pressure + "hPa";
+    //   document.getElementById("data_sunset").innerHTML = new Date(data.sys.sunset*1000).toLocaleTimeString();
+    // ;
+    let backgroundImage;
+    let textColor;
+    let weatherArray = ['rain', 'clouds', 'clear', 'mist']
+    let weatherName = weatherData[1].toLowerCase()
+    backgroundImage = `url('../assets/${weatherName}.jpg')`
+    if(!(weatherArray.includes(weatherName))) backgroundImage = '#111827'
 
-  document.getElementById("temperature").innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°C";
-  document.getElementById("location").innerHTML = data.name;
-  document.getElementById("description").innerHTML = data.weather[0].description;
-  document.getElementById("data_city").innerHTML = data.name;
-  document.getElementById("data_temperature").innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°C";
-  document.getElementById("data_humidity").innerHTML = data.main.humidity + "%";
-  document.getElementById("data_wind_speed").innerHTML = data.wind.speed + "m/s";
-  document.getElementById("data_wind_direction").innerHTML = data.wind.deg + "º";
-  document.getElementById("data_pressure").innerHTML = data.main.pressure + "hPa";
-  document.getElementById("data_sunrise").innerHTML = new Date(data.sys.sunrise*1000).toLocaleTimeString();
-  document.getElementById("data_sunset").innerHTML = new Date(data.sys.sunset*1000).toLocaleTimeString();
-;
-}
+  }
