9d829fe7013f11cdbdb5dee41d4f8afa17c4d8a8
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-11-17 03:18:17 +0100

    Change icons to black variants

--- js/main.js
+++ js/main.js
@@ -21,11 +21,13 @@ let weatherIcon = dq('[data-weatherIcon]')
 let country = dq('[data-country]')
 let gif = dq('[data-gif]')
 let loader = dq('[data-loader]')
-
+let loading = dq('.loader')
+let redirect
 
 window.onload = function(){
   loadSite('London')
 }
+
 
 searchForm.addEventListener('submit', (e) =>{
   e.preventDefault()
@@ -44,21 +46,29 @@ function submitQuery(){
 
 
 
+
+
+
+
 async function loadSite(cityName){
-    loader.style.opacity = '1'
-    let weatherData = await getRequest(cityName)
-    if (weatherData === 400) {
-      alert('City Not Found')
-      return
-    }
-    
+  
+  
+  loader.classList.add('transition-none')
+  loader.style.opacity = '1'
+  loader.style.display = 'flex'
+  let weatherData = await getRequest(cityName)
+  if (weatherData === 400) {
+    alert('City Not Found. Go To Default')
+    loadSite('LONDON')
+    return
+  }
     let backgroundGif = await getGif(weatherData[1])
     let data = [weatherData, backgroundGif]
     updatePage(data)
-}
-
-
-function updatePage(data){
+  }
+  
+  
+  function updatePage(data){
     let weatherData = data[0]
     let backgroundGif = data[1]
     data = weatherData[0]
@@ -70,10 +80,10 @@ function updatePage(data){
     backgroundImage = `url('../assets/${weatherName}.jpg')`
     if(!(weatherArray.includes(weatherName))) backgroundImage = '#111827'
     root.style.setProperty('--bg-image', backgroundImage)
-   
+    
     cityName.innerHTML = data.name;
     time.innerHTML = `Today ${today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
-    temperature.innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°";
+    temperature.innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°C";
     weatherIcon.src = "http://openweathermap.org/img/w/" + iconCode + ".png"
     weatherIcon.style.display = 'inline-block'
     weather.innerHTML = weatherData[1]
@@ -81,17 +91,7 @@ function updatePage(data){
     humidity.innerHTML = data.main.humidity + '%'
     windSpeed.innerHTML = data.wind.speed + 'm/s'
     gif.src = backgroundGif
-    setTimeout(() => {
-      loader.style.opacity = '0'
-    }, 1000)
-    //   document.getElementById("location").innerHTML = data.name;
-    //   document.getElementById("description").innerHTML = data.weather[0].description;
-    //   document.getElementById("data_temperature").innerHTML = Math.round((data.main.temp-273.15)*10)/10 + "°C";
-    //   document.getElementById("data_humidity").innerHTML = data.main.humidity + "%";
-    //   document.getElementById("data_wind_speed").innerHTML = data.wind.speed + "m/s";
-    //   document.getElementById("data_wind_direction").innerHTML = data.wind.deg + "º";
-    //   document.getElementById("data_pressure").innerHTML = data.main.pressure + "hPa";
-    //   document.getElementById("data_sunset").innerHTML = new Date(data.sys.sunset*1000).toLocaleTimeString();
-    // ;
-
+    loader.classList.remove('transition-none')
+    loader.style.opacity = '0'
+    
   }
